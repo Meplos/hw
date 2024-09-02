9561b23e81ed2b41a3ebbf01abc483ecd620ab2b
Meplos <alexxd64150@gmail.com>
2024-09-02 15:50:36 +0200

    fix: copy error for directory

--- start.sh
+++ start.sh
@@ -3,5 +3,5 @@
 cd ./app && npm i && npm run build
 cd ../server && npm i && npm run build
 mkdir -p ./build/src/public
-cp ../app/dist/* ./build/src/public/
+cp -r ../app/dist/* ./build/src/public/
 npm run serve
